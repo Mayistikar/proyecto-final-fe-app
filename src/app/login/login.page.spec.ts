72af33e6209446ce3d268d46e4c58359ed5230f9
Mayistikar <andersonrodriguezce@gmail.com>
2025-03-31 21:41:32 -0500

    fixing tests

--- src/app/login/login.page.spec.ts
+++ src/app/login/login.page.spec.ts
@@ -1,11 +1,28 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute, Router } from '@angular/router';
+import { of } from 'rxjs';
 import { LoginPage } from './login.page';
 
 describe('LoginPage', () => {
   let component: LoginPage;
   let fixture: ComponentFixture<LoginPage>;
 
-  beforeEach(() => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [LoginPage],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            params: of({}),
+            snapshot: { paramMap: { get: () => 'test-id' } }
+          }
+        },
+        Router,
+      ]
+    })
+    .compileComponents();
+
     fixture = TestBed.createComponent(LoginPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
