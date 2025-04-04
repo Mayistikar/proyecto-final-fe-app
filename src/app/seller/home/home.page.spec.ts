dcf02a738c92c248db3c33ef9cd6fd986271386a
Jader Pelaez <j.pelaezg@uniandes.edu.co>
2025-04-04 12:34:06 -0500

    test home page corregido

--- src/app/seller/home/home.page.spec.ts
+++ src/app/seller/home/home.page.spec.ts
@@ -1,11 +1,30 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HomePage } from './home.page';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
 
 describe('HomePage', () => {
   let component: HomePage;
   let fixture: ComponentFixture<HomePage>;
 
-  beforeEach(() => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [HomePage],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            params: of({}), // puedes simular parámetros si es necesario
+            snapshot: {
+              paramMap: {
+                get: (key: string) => null
+              }
+            }
+          }
+        }
+      ]
+    }).compileComponents();
+
     fixture = TestBed.createComponent(HomePage);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -15,3 +34,5 @@ describe('HomePage', () => {
     expect(component).toBeTruthy();
   });
 });
+
+
