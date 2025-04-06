283d92af2b24c919db7b70a5b8dcae1367d6610e
Mayistikar <andersonrodriguezce@gmail.com>
2025-04-06 14:57:21 -0500

    Order Creation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -37,7 +37,7 @@ const routes: Routes = [
   {
     path: 'deliveries',
     loadComponent: () => import('./client/deliveries/deliveries.component').then( m => m.DeliveriesComponent),
-    // canActivate: [RoleGuard],
+    canActivate: [RoleGuard],
     data: { expectedRole: 'client' } // Rol esperado para esta ruta
   },
   {
